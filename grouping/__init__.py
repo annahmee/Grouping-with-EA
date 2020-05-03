bd3e2c6f7480517319c8a896dc311b1661164beb
zmp <1399455817@qq.com>
2020-05-03 19:27:51 +0800

    add dotenv

--- grouping/__init__.py
+++ grouping/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask,request,render_template,flash
 
-
 def create_app():
     app=Flask('grouping')
-    app.secret_key='test'
+    
+    app.config.from_pyfile('settings.py')
 
     from . import auth
     app.register_blueprint(auth.bp)
     from . import grouping
     app.register_blueprint(grouping.bp)
 
-    
     return app
 
 
